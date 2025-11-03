31d967a38615beda228da2163f4f1b7bba1f5a9c
Sohag Hossain <125116828+Sohag264@users.noreply.github.com>
2025-11-03 23:53:53 +0600

    আপডেট

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="bg-dark text-white text-center py-3">
-    <p>&copy; 2019-2024. <br> Design and Developed By: Sohag.</p>
-</footer>
--- header.php
+++
@@ -1,3 +0,0 @@
-<header class="bg-dark text-white text-center py-4">
-    <h1>S-Kit 1.0</h1>
-</header>
